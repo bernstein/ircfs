78cfab8f5ac4183c7229d57c62081f134657510a
Andreas-Christoph Bernstein <andreas.bernstein@googlemail.com>
2011-11-01 23:44:04 +0100

    added command line processing

---
+++ Ircfs/CmdLine.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE DeriveDataTypeable, GeneralizedNewtypeDeriving #-}
+-- -----------------------------------------------------------------------------
+-- |
+-- Module      :  Ircfs.CmdLine
+-- Copyright   :  (c) Andreas-Christoph Bernstein 2011
+-- License     :  BSD3-style (see LICENSE)
+--
+-- Maintainer  :  andreas.bernstein@googlemail.com
+-- Stability   :  unstable
+-- Portability :  not portable
+--
+-- Command line options
+--
+--------------------------------------------------------------------------------
+
+module Ircfs.CmdLine
+where
+
+import System.Console.CmdArgs -- (Data, Typeable)
+
+showVersion = "0.0.1"
+
+data Config = Config 
+            { nick :: String
+            , fromhost :: String
+            , addr :: String
+            , port :: String
+            , secret :: String
+            , logpath :: FilePath
+            , mtpt :: FilePath
+            } deriving (Show, Data, Typeable)
+
+conf = Config
+  { nick = def &= args &= typ "NICKNAME"
+  , fromhost = def &= help "fromhost" &= typ "FROMHOST"
+  , addr = "irc.freenode.net" &= help "server" &= typ "SERVER"
+  , port = "6667" &= help "port" &= typ "PORT"
+  , secret = def &= help "port number" &= typ "PASSWORD"
+  , logpath = def &= help "Path to logfile" &= typFile
+  , mtpt = "/mnt/irc" &= help "Mount point" &= typDir
+  } &= summary ("ircfs v" ++ showVersion ++ ", (C) Andreas-C. Bernstein 2011\n")
+    &= program "ircfs"
+
+
+cmdLine :: IO Config
+cmdLine = cmdArgs conf
+
