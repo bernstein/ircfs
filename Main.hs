3b04f9d835a942de6369628fb163e910251a6a9f
Andreas-Christoph Bernstein <andreas.bernstein@googlemail.com>
2011-11-05 23:44:28 +0100

    newtype wrapper around channels. simplifications

--- Main.hs
+++ Main.hs
@@ -12,9 +12,12 @@ import System.Posix.Files
 import System.Posix.IO
 import System.FilePath (splitFileName,takeBaseName)
 import System.Environment (withArgs)
+import System.Locale (defaultTimeLocale)
+import qualified Data.Time as T
+import qualified Data.Time.Format as T
 import qualified System.Fuse as F
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad (foldM_)
+import Control.Monad.IO.Class (liftIO, MonadIO)
+import Control.Monad (foldM_,mapM_)
 import Data.Maybe (maybe)
 import qualified Control.Concurrent.Chan as C
 import qualified Control.Concurrent as C
@@ -30,29 +33,97 @@ import qualified Network.Socket as N hiding (recv)
 import qualified Network.Socket.ByteString  as N (recv, sendAll)
 
 import qualified Network.IRC.Message as I
+import Network.IRC.Enumerator
 import Ircfs.Ctl as I
 import Ircfs.Types
+import Ircfs.Filesystem
 import qualified Ircfs.CmdLine as O
 import System.Fuse.Request
 
-helloString = "it works!!!!!!!"
+data In  = Fs  Request
+         | Cmd CtlCommand
+         | Irc I.Message
+         | Shutdown
+         -- deriving (Eq)
 
-newtype IrcOut = IrcOut { unIrcOut :: C.Chan I.Message }
-newtype IrcIn = IrcIn { unIrcIn :: C.Chan I.Message }
+--newtype IrcOut = IrcOut { unIrcOut :: C.Chan I.Message }
+newtype IrcOut = IrcOut { unIrcOut :: C.Chan B.ByteString }
+newtype IrcIn  = IrcIn { unIrcIn :: C.Chan I.Message }
 
-processFuseRequests :: IrcOut -> IrcfsState -> Request -> IO IrcfsState
-processFuseRequests ircoutc st (ReqRep t m) = 
-    fmap snd $ runIrcfs st $ processTmsg ircoutc t >>= (io . C.putMVar m)
-processFuseRequests ircoutc st (Req t) = 
-    fmap snd $ runIrcfs st $ processTmsg ircoutc t
+-- process :: Enumeratee In I.Message IO a
+
+--process :: IrcfsState -> In -> IO (Maybe B.ByteString, IrcfsState)
+--process :: IrcOut -> IrcfsState -> In -> IO IrcfsState
+process :: IrcOut -> In -> Ircfs ()
+-- process file system requests
+process ircoutc (Fs (ReqRep t m)) = processTmsg ircoutc t >>= (io . C.putMVar m)
+process ircoutc (Fs (Req t)) = processTmsg ircoutc t >> return ()
+
+-- /event -- new x #channel, new y user, del x #channel
+--
+
+--doIRC :: MonadIO m => IrcOut -> N.Socket -> I.Message -> m ()
+-- ircoutc is actually not needed as an argument
+-- TODO just write a PONG statement as System.Fuse.Request to Qrootctl
+--doIRC fsReq ircoutc (I.Message p I.NOTICE ps) = undefined
+--doIRC fsReq ircoutc (I.Message p I.PRIVMSG ps) = undefined
+--doIRC fsReq ircoutc (I.Message p I.NICK ps) = undefined
+--doIRC fsReq ircoutc (I.Message p I.MODE ps) = undefined
+--doIRC fsReq ircoutc (I.Message p I.QUIT ps) = undefined
+--doIRC fsReq ircoutc (I.Message p I.ERROR ps) = undefined
+--doIRC fsReq ircoutc (I.Message p I.SQUIT ps) = undefined
+--doIRC fsReq ircoutc (I.Message p I.JOIN ps) = undefined
+--doIRC fsReq ircoutc (I.Message p I.PART ps) = undefined
+--doIRC fsReq ircoutc (I.Message p I.KICK ps) = undefined
+--doIRC fsReq ircoutc (I.Message p I.TOPIC ps) = undefined
+--doIRC fsReq ircoutc (I.Message p I.INVITE ps) = undefined
+-- process incoming irc messages
+process ircoutc (Irc m@(I.Message _ I.PING ps)) = do
+    stamp <- timeStamp
+    let cmd = "pong " `B.append` head ps `B.append` "\n"
+        off = fromIntegral . B.length $ cmd
+        log = stamp `B.append` " " `B.append` cmd
+        s = I.toByteString m
+        off2 = fromIntegral . B.length $ s
+    processTmsg ircoutc (Twrite "/ctl" cmd off)
+    processTmsg ircoutc (Twrite "/pong" log off)
+    writeRaw s off2
+process ircoutc (Irc m@(I.Message (Just (I.PrefixNick n _ _)) I.NICK (new:_))) = do
+    st <- get
+    if n == nick (connection st)
+      then do
+        let log = "your nick changed\n"
+            off = fromIntegral . B.length $ log
+        processTmsg ircoutc (Twrite "/event" log off) >> return ()
+        processTmsg ircoutc (Twrite "/nick" new (fromIntegral . B.length $ new))
+        return ()
+      else do
+        let log = "someones nick changed\n"
+            off = fromIntegral . B.length $ log
+        processTmsg ircoutc (Twrite "/event" log off)
+        return ()
+    let s = I.toByteString m
+        off2 = fromIntegral . B.length $ s
+    writeRaw s off2
+
+process ircoutc (Irc m@(I.Message _ I.ERROR ps)) =
+    let s = I.toByteString m
+        off = fromIntegral . B.length $ s
+    in writeRaw s off
+process ircoutc (Irc m) =
+    let s = I.toByteString m
+        off = fromIntegral . B.length $ s
+    in writeRaw s off
+process _ _ = return ()
 
 processTmsg :: IrcOut -> Tmsg -> Ircfs Rmsg
 -- process Tread
 processTmsg _ (Tread "/nick" byteCount offset) = do
   return . Rread . B.take (fromIntegral byteCount) 
-        . B.drop (fromIntegral offset) . nick . connection =<< get
+        . B.drop (fromIntegral offset) . (`B.append` "\n") . nick
+        . connection =<< get
 processTmsg _ (Tread "/pong" byteCount offset) = do
-  return . Rread . B.take (fromIntegral byteCount) 
+  return . Rread . B.take (fromIntegral byteCount)
         . B.drop (fromIntegral offset) . pongFile . connection =<< get
 processTmsg _ (Tread "/raw" byteCount offset) = do
   return . Rread . B.take (fromIntegral byteCount) 
@@ -60,83 +131,51 @@ processTmsg _ (Tread "/raw" byteCount offset) = do
 processTmsg _ (Tread "/event" byteCount offset) =
   return . Rread . B.take (fromIntegral byteCount) 
         . B.drop (fromIntegral offset) . eventFile . connection =<< get
+processTmsg _ (Tread "/0/name" byteCount offset) = do
+  return . Rread . B.take (fromIntegral byteCount)
+        . B.drop (fromIntegral offset) . (`B.append` "\n"). addr
+        . connection =<< get
 processTmsg _ (Tread {}) = return Rerror
 
 -- process Twrite
 processTmsg ircoutc (Twrite "/ctl" s offset) = do
-  st <- get
-  let m = case A.parse I.parseCtl s of
-         A.Fail {} -> Nothing
-         A.Partial {} -> Nothing
-         A.Done _ (I.Unknown {}) -> Nothing
-         A.Done _ c -> Just (toMessage c)
   -- Todo if a msg is longer than 512 then split it into chunks
-  maybe (return ()) (io . C.writeChan (unIrcOut ircoutc)) m
-  let r = Rwrite . fromIntegral . B.length $ s
-  return r
-processTmsg ircoutc (Twrite "/event" s offset) = do
+  maybe (return Rerror)
+        (\c -> processTmsg ircoutc (Twrite "/raw" (I.toByteString.toMessage $c) 0)) (A.maybeResult $ A.parse I.parseCtl s)
+  return . Rwrite . fromIntegral . B.length $ s
+processTmsg _ (Twrite "/event" s offset) = do
   modify $ L.modL (eventLens.connectionLens) (`B.append` s)
   return . Rwrite . fromIntegral . B.length $ s
-processTmsg ircoutc (Twrite "/pong" s offset) = do
+processTmsg _ (Twrite "/nick" s offset) = do
+  modify $ L.setL (nickLens.connectionLens) s
+  return . Rwrite . fromIntegral . B.length $ s
+processTmsg _ (Twrite "/pong" s offset) = do
   modify $ L.modL (pongLens.connectionLens) (`B.append` s)
   return . Rwrite . fromIntegral . B.length $ s
 processTmsg ircoutc (Twrite "/raw" s offset) = do
   modify $ L.modL (rawLens.connectionLens) (`B.append` s)
+  -- XXX writes to /raw are special,
+  -- this part is the only reason processTmsg neds ircoutc
+  io . C.writeChan (unIrcOut ircoutc) $ s
   return . Rwrite . fromIntegral . B.length $ s
-processTmsg ircoutc (Twrite {}) = return Rerror
+processTmsg _ (Twrite {}) = return Rerror
 
 -- process Topen
 processTmsg _ (Topen p _ _) = return Ropen
-  -- | takeBaseName p == "ctl"  = return Rerror
-  -- | otherwise   = return Ropen
 
 -- process Tstat
-processTmsg _ (Tstat p) = do
-  st <- get
-  let m = maybe Rerror Rstat (stat st p)
-  return m
+processTmsg _ (Tstat p) = maybe Rerror Rstat . (`stat` p) <$> get
 
-stat :: IrcfsState -> FilePath -> Maybe F.FileStat
-stat (IrcfsState NotConnected) _ = Nothing
-stat (IrcfsState con) "/nick" = 
-  let s = (fileStat Qnick) { F.statFileSize = size }
-      size = fromIntegral . B.length . nick $ con
-  in  Just s
-stat (IrcfsState con) "/event" = 
-  let size = fromIntegral . B.length . eventFile $ con
-  in  Just $ (fileStat Qevent) { F.statFileSize = size }
-stat (IrcfsState con) "/raw" = 
-  let size = fromIntegral . B.length . rawFile $ con
-  in  Just $ (fileStat Qevent) { F.statFileSize = size }
-stat (IrcfsState con) "/pong" = 
-  let size = fromIntegral . B.length . pongFile $ con
-  in  Just $ (fileStat Qevent) { F.statFileSize = size }
-stat (IrcfsState con) p = fileStat <$> fromFilePath p
+writeRaw :: B.ByteString -> Int -> Ircfs ()
+writeRaw s off = modify $ L.modL (rawLens.connectionLens) (`B.append` s)
 
 chanToIter2 :: C.Chan a -> E.Iteratee a IO ()
 chanToIter2 chan = go
   where
     go = EL.head >>= maybe go (\x -> liftIO (C.writeChan chan x) >> go)
 
-iterMessage :: Monad m => E.Iteratee B.ByteString m I.Message
-iterMessage = A.iterParser I.message
-
-messages :: Monad m => E.Enumeratee B.ByteString I.Message m a
-messages = E.sequence iterMessage
-
---doIRC :: MonadIO m => IrcOut -> N.Socket -> I.Message -> m ()
--- ircoutc is actually not needed as an argument
--- TODO just write a PONG statement as System.Fuse.Request to Qrootctl
-doIRC fsReq ircoutc (I.Message _ I.PING ps) = do
-  let msg = (I.Message Nothing I.PONG ps)
-      s = I.toByteString msg
-      off = fromIntegral . B.length $ s
-  liftIO $ C.writeChan (unIrcOut ircoutc) msg
-  fuseRequest_ fsReq (Twrite "/pong" s off) 
-doIRC fsReq ircoutc m = do
-  let s = I.toByteString m
-      off = fromIntegral . B.length $ s
-  fuseRequest_ fsReq (Twrite "/raw" s off) 
+-- writeraw fsReq s = fuseRequest_ fsReq $ Twrite "/raw" s (B.length s)
+-- writemsg
 
 -- listens on the sockets, writes received messages to ircinc
 ircReader :: N.Socket -> IrcIn -> IO ()
@@ -145,17 +184,16 @@ ircReader sock ircinc = do
   return ()
 
 ircWriter :: N.Socket -> IrcOut -> IO ()
-ircWriter sock ircoutc = do
+ircWriter s ircoutc = do
     ms <- C.getChanContents (unIrcOut ircoutc)
-    mapM_ (N.sendAll sock . I.toByteString) ms
+    --mapM_ (N.sendAll s . I.toByteString) ms
+    mapM_ (N.sendAll s) ms
 
 fsInit :: C.Chan Request -> O.Config -> IO ()
 fsInit fsReq cfg = do
+  inc <- C.newChan :: IO (C.Chan In)
   ircoutc <- IrcOut <$> C.newChan
-  addrinfos <- N.getAddrInfo Nothing (Just (O.addr cfg)) (Just (O.port cfg))
-  let serveraddr = head addrinfos
-  sock <- N.socket (N.addrFamily serveraddr) N.Stream N.defaultProtocol
-  N.connect sock (N.addrAddress serveraddr)
+  s <- getSocket (O.addr cfg) (read (O.port cfg))
 
   let st = IrcfsState 
             { connection = 
@@ -163,7 +201,7 @@ fsInit fsReq cfg = do
                       { addr = B.pack . O.addr $ cfg
                       , nick = B.pack . O.nick $ cfg
                       , targets = mempty
-                      , sock = sock
+                      , sock = s
                       , eventFile = ""
                       , pongFile = ""
                       , rawFile = ""
@@ -172,19 +210,22 @@ fsInit fsReq cfg = do
             }
 
   ircinc <- IrcIn <$> C.newChan
-  C.forkIO $ ircReader sock ircinc
+  C.forkIO $ ircReader s ircinc
 
   let nickMsg = B.pack $ "NICK " ++ O.nick cfg ++ "\r\n" 
-  N.sendAll sock nickMsg 
+  N.sendAll s nickMsg
   let userMsg = B.pack $ "USER none 0 * :" ++ O.nick cfg ++"\r\n"
-  N.sendAll sock userMsg
-  C.forkIO $ ircWriter sock ircoutc
+  N.sendAll s userMsg
+  C.forkIO $ ircWriter s ircoutc
 
   ms <- C.getChanContents (unIrcIn ircinc)
-  C.forkIO $ mapM_ (doIRC fsReq ircoutc) ms
+  C.forkIO $ foldM_ (\c m -> C.writeChan c (Irc m) >> return c) inc ms
 
   rs <- C.getChanContents fsReq
-  C.forkIO $ foldM_ (processFuseRequests ircoutc) st rs
+  C.forkIO $ foldM_ (\c r -> C.writeChan c (Fs r) >> return c) inc rs
+
+  is <- C.getChanContents inc
+  C.forkIO $ runIrcfs st (mapM_ (process ircoutc) is) >> return ()
   return ()
 
 fsDestroy :: IO ()
@@ -208,124 +249,25 @@ main = N.withSocketsDo $ do
             }
   withArgs [O.mtpt args] $ F.fuseMain ops F.defaultExceptionHandler
 
--- fuseInit :: FuseOperations fh -> IO ()
--- fuseOpen :: FilePath -> OpenMode -> OpenFileFlags -> IO (Either Errno fh)
--- On success, returns Right of a filehandle-like value that will be passed to 
--- future file operations; on failure, returns Left of the appropriate Errno.
---
--- No creation, exclusive access or truncating flags will be passed. This 
--- should check that the operation is permitted for the given flags. 
---
--- fuseRead :: FilePath -> fh -> ByteCount -> FileOffset -> 
---                 IO (Either Errno ByteString)
---
--- fuseWrite :: FilePath -> fh -> ByteString -> FileOffset -> IO (Either Errno ByteCount)
--- fuseGetFileSystemStats :: String -> IO (Either Errno FileSystemStats)
--- implements statfs(2)
--- fuseOpenDirectory :: FilePath -> IO Errno
--- This method should check if the open operation is permitted for this directory. 
--- fuseReadDirectory :: FilePath -> IO (Either Errno [(FilePath, F.FileStat)])
--- fuseAccess :: FilePath -> Int -> IO Errno
--- fuseGetFileStat :: FilePath -> IO (Either Errno F.FileStat)
---   lstat
--- 
-
 fsReadDir :: FilePath -> IO (Either Errno [(FilePath, F.FileStat)])
-fsReadDir p@"/" = return . Right $ 
+fsReadDir "/" = return . Right $
                     [(".", defaultDirStat) ,("..", defaultDirStat), ("0",defaultDirStat) ] ++ rootDir
   where
     rootDir = zip (map showFilepath rootDirFiles) (map fileStat rootDirFiles)
-fsReadDir p = return . Right $ 
+fsReadDir _ = return . Right $
                     [(".", defaultDirStat), ("..",defaultDirStat)] ++ subDir
   where
     subDir = zip (map showFilepath subDirFiles) (map fileStat subDirFiles)
 --fsReadDir _ = return (Left (eNOENT))
 
 fsOpenDirectory :: FilePath -> IO Errno
-fsOpenDirectory  "/" = return eOK
-fsOpenDirectory  p = return eOK
-
-{-
-fsOpen :: FilePath -> OpenMode -> OpenFileFlags -> IO (Either Errno IrcfsFH)
-fsOpen p m f = 
-  | p == "/ctl" = return (Right IrcfsFH)
-  | p == "/nick" = return (Right IrcfsFH)
-  | otherwise = return (Left eNOENT)
--}
+fsOpenDirectory  = const (return eOK)
 
 fsTruncate :: FilePath -> FileOffset -> IO Errno
-fsTruncate p off = if takeBaseName p == "ctl" then return eOK else return eACCES
+fsTruncate p _ = if takeBaseName p == "ctl" then return eOK else return eACCES
 
-showFile :: Qreq -> B.ByteString
-showFile Qroot    = "/"
-showFile Qrootctl = "ctl"
-showFile Qevent   = "event"
-showFile Qraw     = "raw"
-showFile Qnick    = "nick"
-showFile Qpong    = "pong"
-showFile Qdir     = ""
-showFile Qctl     = "ctl"
-showFile Qname    = "name"
-showFile Qusers   = "users"
-showFile Qdata    = "data"
+timeStamp :: MonadIO m => m B.ByteString
+timeStamp = do
+  now <- liftIO $ T.getCurrentTime
+  return . B.pack $ (T.formatTime defaultTimeLocale "%H:%M" now)
 
-showFilepath :: Qreq -> FilePath
-showFilepath = B.unpack . showFile
-
-filemode :: Qreq -> FileMode
-filemode Qroot    = 0o555 -- DIR
-filemode Qrootctl = 0o222
-filemode Qevent   = 0o444
-filemode Qraw     = 0o666
-filemode Qnick    = 0o444
-filemode Qpong    = 0o444
-filemode Qdir     = 0o555 -- DIR
-filemode Qctl     = 0o222
-filemode Qname    = 0o444
-filemode Qusers   = 0o444
-filemode Qdata    = 0o666
-
-fileStat :: Qreq -> F.FileStat
-fileStat Qroot    = defaultDirStat  { F.statFileMode = filemode Qroot }
-fileStat Qrootctl = defaultFileStat { F.statFileMode = filemode Qrootctl }
-fileStat Qevent   = defaultFileStat { F.statFileMode = filemode Qevent }
-fileStat Qraw     = defaultFileStat { F.statFileMode = filemode Qraw }
-fileStat Qnick    = defaultFileStat { F.statFileMode = filemode Qnick }
-fileStat Qpong    = defaultFileStat { F.statFileMode = filemode Qpong }
-fileStat Qdir     = defaultDirStat  { F.statFileMode = filemode Qroot }
-fileStat Qctl     = defaultFileStat { F.statFileMode = filemode Qrootctl }
-fileStat Qname    = defaultFileStat { F.statFileMode = filemode Qname }
-fileStat Qusers   = defaultFileStat { F.statFileMode = filemode Qusers }
-fileStat Qdata    = defaultFileStat { F.statFileMode = filemode Qdata }
-
-toQreq :: String -> Maybe Qreq
-toQreq "ctl"   = Just Qctl
-toQreq "event" = Just Qevent
-toQreq "nick"  = Just Qnick
-toQreq "raw"   = Just Qraw
-toQreq "pong"  = Just Qpong
-toQreq "data"  = Just Qdata
-toQreq "name"  = Just Qname
-toQreq "users" = Just Qusers
-toQreq _       = Nothing
-
-fromFilePath :: FilePath -> Maybe Qreq
-fromFilePath "/"    = Just Qroot
-fromFilePath "/ctl" = Just Qrootctl
-fromFilePath "/0"   = Just Qdir
-fromFilePath p      = toQreq . snd . splitFileName $ p
-
-rootDirFiles :: [Qreq]
-rootDirFiles = [Qrootctl, Qevent, Qnick, Qraw, Qpong]
-
-subDirFiles :: [Qreq]
-subDirFiles = [Qctl, Qdata, Qname, Qusers]
-
-
-files :: [Qreq]
-files = [ Qroot, Qrootctl, Qevent, Qraw, Qnick, Qpong, Qdir, Qctl, Qname
-        , Qusers, Qdata
-        ]
-
---connect :: -> IO Connection
---
