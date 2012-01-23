a1624cd438f75d639d12920bb8f225f4620512c5
Andreas-Christoph Bernstein <andreas.bernstein@googlemail.com>
2012-01-23 19:59:36 +0100

    changed import stmt

--- Network/IRC/Message/Parser.hs
+++ Network/IRC/Message/Parser.hs
@@ -23,7 +23,7 @@ module Network.IRC.Message.Parser
   ) where
 
 import           Prelude hiding (takeWhile)
-import           Control.Applicative hiding (many)
+import           Control.Applicative -- hiding (many)
 import           Data.Attoparsec
 import qualified Data.Attoparsec.Char8 as P8
 import           Data.Attoparsec.Char8 (char8)
