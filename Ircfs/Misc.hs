c1e09295cb197643a8bce55423e377879662ebd5
Andreas-Christoph Bernstein <andreas.bernstein@googlemail.com>
2012-02-04 14:49:41 +0100

    added misc

--- Ircfs/Misc.hs
+++ Ircfs/Misc.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings, BangPatterns #-}
 -- -----------------------------------------------------------------------------
 -- |
 -- Module      :  Ircfs.Process
@@ -12,6 +13,7 @@
 --------------------------------------------------------------------------------
 module Ircfs.Misc
 --  ( 
+--  , timeStamp
 --  , now
 --  ) where
 where
@@ -20,10 +22,19 @@ import Prelude hiding ((.), id)
 import Control.Category
 import Foreign.C.Types (CTime)
 import Control.Applicative
+import Control.Monad.IO.Class (liftIO, MonadIO)
 import qualified Data.Time as T
 import qualified Data.Time.Clock.POSIX as T
 import System.Locale (defaultTimeLocale)
-import Data.List (partition)
+import qualified Data.ByteString.Lazy.Char8 as BL hiding (map)
+import qualified Data.ByteString.Lazy as BL hiding (elemIndex,head)
+import qualified Data.ByteString.Char8 as B
+import           Data.List (partition)
+import           Data.Monoid
+import           Data.IORef
+import qualified Data.Time as T
+import System.Locale (defaultTimeLocale)
+import Foreign.C.Types (CTime)
 
 type Endomorphism a = a -> a
 
@@ -50,3 +61,42 @@ minfrom a (n,xs)
     where (us,vs) = partition (<b) xs
           b = a + 1 + n `div` 2
           m = length us
+
+breakAfter :: BL.ByteString -> BL.ByteString -> (BL.ByteString, BL.ByteString)
+breakAfter str bs | BL.null str = (bs,mempty)
+breakAfter str bs = find 0 bs
+  where find n xs =
+          case BL.elemIndex c xs of
+            Nothing -> (bs,mempty)
+            Just i ->
+                    let (_,rest) = BL.splitAt (n+i) bs
+                    in if str `BL.isPrefixOf` rest then BL.splitAt (n+i+2) bs
+                       else find (n+i+1) rest
+        c = BL.head str
+
+split :: BL.ByteString -> BL.ByteString -> [BL.ByteString]
+split e bs = xs : if BL.null ys then [] else split e ys
+     where (xs,ys) = breakAfter e bs
+
+breakAfterCRLF :: BL.ByteString -> (BL.ByteString, BL.ByteString)
+breakAfterCRLF bs = find 0 bs
+  where find n xs =
+          case BL.elemIndex '\r' xs of
+            Nothing -> (bs,mempty)
+            Just i ->
+                    let (_,rest) = BL.splitAt (n+i) bs
+                    in if ("\r\n") `BL.isPrefixOf` rest then BL.splitAt (n+i+2) bs
+                       else find (n+i+1) rest
+
+timeStamp :: MonadIO m => m B.ByteString
+timeStamp = do
+  now <- liftIO T.getCurrentTime
+  return . B.pack $ T.formatTime defaultTimeLocale "%H:%M" now
+
+timeStamp' :: CTime -> B.ByteString
+timeStamp' = B.pack . stamp . toUTCTime
+
+atomicModifyIORef_ :: IORef a -> (a -> a) -> IO ()
+atomicModifyIORef_ ref f = do
+  !_ <- atomicModifyIORef ref (\st -> (f st,()))
+  return ()
